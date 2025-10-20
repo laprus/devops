a60abe09fd0ad13bd304bf8969ce800f024f652f
Pawel Laprus <pawel.laprus@hyland.com>
2025-10-20 12:37:56 +0200

    fix: healthy

--- deployments/terraform/locals.tf
+++ deployments/terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
   resource_suffix = var.deploy_as_feature_branch ? "-${var.branch_hash}" : ""
-  bucket_name     = "cin-devops-${var.stack_environment}${local.resource_suffix}-v4.x.x"
+  bucket_name     = "cin-devops-${var.stack_environment}${local.resource_suffix}-v5.x.x"
 }
