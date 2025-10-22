fddbf6e2ce9b8913ac5f03601d9ab9e3601c432d
Pawel Laprus <pawel.laprus@hyland.com>
2025-10-22 11:40:33 +0200

    feat: demo purpose

--- deployments/terraform/locals.tf
+++ deployments/terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
   resource_suffix = var.deploy_as_feature_branch ? "-${var.branch_hash}" : ""
-  bucket_name     = "cin-devops-${var.stack_environment}${local.resource_suffix}-8.3.0"
+  bucket_name     = "cin-devops-${var.stack_environment}${local.resource_suffix}-8.4.0"
 }
